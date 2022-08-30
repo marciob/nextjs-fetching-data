7a6a2fdd6ecf90aeefdbd3477252af2d59ad1867
m <13090422+marciob@users.noreply.github.com>
2022-08-30 16:34:12 -0300

    first commit

---
+++ actual.js
@@ -0,0 +1,47 @@
+// import path from "path";
+// import fs from "fs/promises";
+import { Fragment } from "react";
+
+function ProductDetailPage(props) {
+  const { loadedProduct } = props;
+
+  return (
+    <Fragment>
+      <h1>{loadedProduct.title}</h1>
+      <p>{loadedProduct.description}</p>
+    </Fragment>
+  );
+}
+
+// export async function getStaticProps(context) {
+//   const { params } = context;
+
+//   const productId = params.id;
+
+//   const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
+//   const jsonData = await fs.readFile(filePath);
+//   const data = JSON.parse(jsonData);
+
+//   const product = data.products.find((product) => product.id === productId);
+
+//   return {
+//     props: {
+//       loadedProduct: product,
+//     },
+//   };
+// }
+
+// export async function getStaticPaths() {
+//   return {
+//     paths: [
+//       //in this example pid is the identifier for the dynamic page [pid].js
+//       //p1, p2 and p3 are the key values from that identifier that will be got by getStaticPaths
+//       { params: { pid: "p1" } },
+//       { params: { pid: "p2" } },
+//       { params: { pid: "p3" } },
+//     ],
+//     fallback: false,
+//   };
+// }
+
+export default ProductDetailPage;
---
+++ pages/[pid].js
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+
+function ProductDetailPage() {
+  return (
+    <Fragment>
+      <h1>TITLE</h1>
+      <p>DESCRIPTION</p>
+    </Fragment>
+  );
+}
+
+export default ProductDetailPage;
